216edd290d7b87e6cf28f4cb9099e2060bf1896a
suprimentosdg <157532344+suprimentosdg@users.noreply.github.com>
2024-01-30 10:55:35 -0300

    Update Relatórios.py

--- Relatórios.py
+++ Relatórios.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 from pymongo import MongoClient
+from io import BytesIO
+from xlsxwriter import Workbook
 
 st.set_page_config(page_title="Relatórios")
 st.title("Portal de Suprimentos")
@@ -17,15 +19,20 @@ with st.container():
         dd=[r for r in dados_mongodb]
         df = pd.DataFrame(dd)
         st.dataframe(df)
-        if st.button("Baixar relatório"):
-            caminho_arquivo = "relatorioEntregas.xlsx"
-            df.to_excel(caminho_arquivo, index=False, sheet_name="RelatorioEntregas")
-            st.success(f"Relatório exportado com sucesso para {caminho_arquivo}")
 
-            with open(caminho_arquivo, "rb") as arquivo:
-                arquivo_bytes = arquivo.read()
+        excel_buffer = BytesIO()
+        with pd.ExcelWriter(excel_buffer, engine="xlsxwriter") as writer:
+            df.to_excel(writer, index=False, header=True)
+        excel_bytes = excel_buffer.getvalue()
 
-            st.write(arquivo_bytes, unsafe_allow_html=True)
+        st.download_button(
+            label="Baixar Relatório",
+            data=excel_bytes,
+            file_name=f"relatórioEntregas.xlsx",
+            key="download_button",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+
     else:
         connectString = "mongodb+srv://suprimentosdglobo:suprimentosdg2023@cluster0.dx7yrgp.mongodb.net/?retryWrites=true&w=majority"
         client = MongoClient(connectString)
@@ -35,12 +42,16 @@ with st.container():
         dd=[r for r in dados_mongodb]
         df = pd.DataFrame(dd)
         st.dataframe(df)
-        if st.button("Baixar relatório"):
-            caminho_arquivo = "relatorioImpressoras.xlsx"
-            df.to_excel(caminho_arquivo, index=False, sheet_name="RelatorioImpressoras")
-            st.success(f"Relatório exportado com sucesso para {caminho_arquivo}")
 
-            with open(caminho_arquivo, "rb") as arquivo:
-                arquivo_bytes = arquivo.read()
+        excel_buffer = BytesIO()
+        with pd.ExcelWriter(excel_buffer, engine="xlsxwriter") as writer:
+            df.to_excel(writer, index=False, header=True)
+        excel_bytes = excel_buffer.getvalue()
 
-            st.write(arquivo_bytes, unsafe_allow_html=True)
+        st.download_button(
+            label="Baixar Relatório",
+            data=excel_bytes,
+            file_name=f"relatórioImpressoras.xlsx",
+            key="download_button",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
