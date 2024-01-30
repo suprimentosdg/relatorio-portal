411d7c31d9384ff9728989b51fa746ab64a0a5d9
suprimentosdg <157532344+suprimentosdg@users.noreply.github.com>
2024-01-30 16:17:30 -0300

    Update Relatórios.py

--- Relatórios.py
+++ Relatórios.py
@@ -84,7 +84,6 @@ with st.container():
             
             df['timestamp'] = pd.to_datetime(df['timestamp'])
 
-            df_filtrado = df.copy()
 
             if data_inicio and data_fim:
                 df_filtrado = df[(df['timestamp'] >= data_inicio) & (df['timestamp'] <= data_fim)]
