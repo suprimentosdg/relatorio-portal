f36a0240a7e94755549f76f9edf05c6f3037aa7f
suprimentosdg <157532344+suprimentosdg@users.noreply.github.com>
2024-02-09 17:31:47 -0300

    Update Relatórios.py

--- Relatórios.py
+++ Relatórios.py
@@ -107,8 +107,9 @@ with st.container():
         dd=[r for r in dados_mongodb]
         df = pd.DataFrame(dd)
         df['timestamp'] = pd.to_datetime(df['timestamp']) - timedelta(hours=3)
+        st.dataframe(df.drop(columns=['id']))
         st.dataframe(df)
-        
+
         countsRegions = df['regional'].value_counts()
         countsRegions_df = pd.DataFrame({'regional': countsRegions.index, 'contagem': countsRegions.values})
 
