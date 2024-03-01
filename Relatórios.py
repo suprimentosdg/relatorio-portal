4f29aa4a2058f731c13f891cabeba495e34a8e03
suprimentosdg <157532344+suprimentosdg@users.noreply.github.com>
2024-03-01 09:37:44 -0300

    Update Relatórios.py

--- Relatórios.py
+++ Relatórios.py
@@ -14,9 +14,10 @@ image = col2.image(image_path, width=80)
 st.subheader("Relatórios")
 st.write('---')
 
-col3, col4, col5 = st.columns([1, 1, 1])
+col3, col4 = st.columns([1, 1])
 options = col3.selectbox("Selecione o relatório desejado:", ["Gerenciamento de Impressoras", "Confirmações de Entregas"])
-with col4.container():
+col5, col6, col7 = st.columns([1, 7, 1])
+with col6.container():
     if  options == "Gerenciamento de Impressoras":
         connectString = "mongodb+srv://suprimentosdglobo:suprimentosdg2023@cluster0.dx7yrgp.mongodb.net/?retryWrites=true&w=majority"
         client = MongoClient(connectString)
